3be0f2740b0cc18e6c2fad2ac0da43c2ee89bc24
LeslieQuet <quetglasleslie@gmail.com>
2023-08-01 16:44:10 -0300

    Mock API & theme for Material

---
+++ src/mockApi.ts
@@ -0,0 +1,50 @@
+export const categories = [
+    {
+      "CategoryId": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales",
+      "Picture": "https://www.paulinacocina.net/wp-content/uploads/2023/06/tragos-con-vodka-y-frutas.jpg",   
+    },
+    {
+      "CategoryId": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings",
+      "Picture": "https://i.blogs.es/a725a3/foto-apertura/450_1000.jpg"
+    },
+    {
+      "CategoryId": 3,
+      "CategoryName": "Confections",
+      "Description": "Desserts, candies, and sweet breads",
+      "Picture": "https://t3.ftcdn.net/jpg/05/11/57/40/360_F_511574067_XcZbe5sekBmMZzwwt5lki1rc8pIkcKhG.jpg"
+    },
+    {
+      "CategoryId": 4,
+      "CategoryName": "Dairy Products",
+      "Description": "Cheeses",
+      "Picture": "https://images.ecestaticos.com/DeyoqxcPACyuhgd4u40iaMOGDU0=/105x55:1719x1266/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F4d1%2Fe95%2F378%2F4d1e9537815f0efde741c9c80d55be32.jpg"
+    },
+    {
+      "CategoryId": 5,
+      "CategoryName": "Grains/Cereals",
+      "Description": "Breads, crackers, pasta and cereal",
+      "Picture": "https://img.europapress.es/fotoweb/fotonoticia_20181002143651_420.jpg"
+    },
+    {
+      "CategoryId": 6,
+      "CategoryName": "Meat/Poultry",
+      "Description": "Prepared meats",
+      "Picture": "https://www.ocu.org/-/media/ocu/images/home/alimentacion/alimentos/embutidos/embutido800x450.jpg?rev=0209a688-4042-4c88-a832-45165c4c643f&hash=55FEC10FE4F6EFDCB2C5A8A1013997C0"
+    },
+    {
+      "CategoryId": 7,
+      "CategoryName": "Produce",
+      "Description": "Dried fruit and bean curd",
+      "Picture": "https://encolombia.com/wp-content/uploads/2021/12/Frutas-Secas.jpg"
+    },
+    {
+      "CategoryId": 8,
+      "CategoryName": "Seafood",
+      "Description": "Seaweed and fish",
+      "Picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFFIrtu4P2HFeChOF0z6E7ZJIW6vV5c-aoz0XptZolYyuGsveIf7_fkICtl6-39A-yh14&usqp=CAU"
+    }
+  ]
